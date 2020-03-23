c33f94acecdfb5d17c8f5b5e290b1acfe31bca3e
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-03-23 22:24:25 +0000

    Fixing linting errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Creates a file called holberotn in /tmp directory
 
-file { "holberton":
-	path    => "/tmp/holberton",
-	ensure  => "file",
-	content => "I love Puppet",
-	group   => "www-data",
-	mode    => "0744",
-	owner   => "www-data",
+file { '/tmp/holberton':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
 }

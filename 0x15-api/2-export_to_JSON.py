2c194a250435c9e8f8fcb115dc7a523fcac950fd
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-05-04 22:42:23 +0000

    Working on task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+This script uses https://jsonplaceholder.typicode.com/
+for a given employee ID, returns information about his/her
+TODO list progress.
+"""
+
+import requests
+import sys
+import json
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        userId = sys.argv[1]
+        usr_url = 'https://jsonplaceholder.typicode.com/users/{}'
+        r = requests.get(usr_url
+                         .format(userId))
+        user = r.json()
+        if user:
+            todos_url = 'https://jsonplaceholder.typicode.com/todos/?userId={}'
+            r = requests.get(todos_url
+                             .format(userId))
+            todos = r.json()
+            user_dict = dict()
+            user_dict[userId] = todos
+            with open('{}.json'.format(userId), 'w') as jsonfile:
+                jsonfile.write(json.dumps(user_dict))

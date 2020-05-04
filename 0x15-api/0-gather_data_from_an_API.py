d907104cf081639d4ad7b70e507e8f393be2db5c
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-05-04 21:46:39 +0000

    Adding isdigit method

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
         userId = sys.argv[1]
         usr_url = 'https://jsonplaceholder.typicode.com/users/{}'
         r = requests.get(usr_url

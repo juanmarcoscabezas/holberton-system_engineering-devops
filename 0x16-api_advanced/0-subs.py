e45c870a0698f80f5563714358f17979530f7197
juanmarcos <juanmarcoscabezas@gmail.com>
2020-05-07 20:09:32 -0500

    Adding comment

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,9 @@ is given, the function should return 0.
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers
+    """
     h = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     r = requests.get(url, headers=h)
